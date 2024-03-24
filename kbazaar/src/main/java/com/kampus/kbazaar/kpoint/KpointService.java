9062c85e2622a4a121e151c7f1f15aba86eabcff
peemapod neamkur <peemapodneamkur@gmail.com>
2024-03-24 11:10:22 +0700

    [Update] Kpoint

--- kbazaar/src/main/java/com/kampus/kbazaar/kpoint/KpointService.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/kpoint/KpointService.java
@@ -4,6 +4,7 @@ import com.kampus.kbazaar.exceptions.NotFoundException;
 import com.kampus.kbazaar.product.Product;
 import com.kampus.kbazaar.product.ProductRepository;
 import com.kampus.kbazaar.product.ProductResponse;
+import com.kampus.kbazaar.shopper.ShopperRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,12 @@ import java.util.Optional;
 
 @Service
 public class KpointService {
+
+    public KpointService(ShopperRepository shopperRepository) {
+        this.shopperRepository = shopperRepository;
+    }
+
+    private ShopperRepository shopperRepository;
 
 
 
